6fdbfbb9aa89852cc87f8b762e1c7cf12990588f
Keith <ko.keith@hotmail.com>
2020-09-18 21:20:23 -0400

    finish twosum

--- TwoSum/TestTwoSum.py
+++ TwoSum/TestTwoSum.py
@@ -6,9 +6,8 @@ class TestPalindrome(unittest.TestCase):
 
     def test_true_palindrome(self):
         twoSumObj = TwoSum()
-
-    def test_false_palindrome(self):
-        twoSumObj = TwoSum()
+        self.assertEqual([2, 3], twoSumObj.twoSum([11, 15, 2, 7], 9))
+        self.assertEqual([0, 1], twoSumObj.twoSum([2, 7, 11, 15], 9))
 
 
 if __name__ == '__main__':
--- TwoSum/TwoSum.py
+++ TwoSum/TwoSum.py
@@ -1,3 +1,16 @@
 class TwoSum(object):
     def twoSum(self, nums, target):
-        pass
+        if len(nums) <= 1:
+            return False
+
+        buff_dict = {}
+
+        # Create a dictionary, with key of target minus current element in nums, and value of current
+        # index. if current element in nums is equal to one of keys in dictionary, return corresponding
+        # indeces
+        for i in range(len(nums)):
+
+            if nums[i] in buff_dict:
+                return [buff_dict[nums[i]], i]
+            else:
+                buff_dict[target - nums[i]] = i
